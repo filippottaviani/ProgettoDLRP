8a8bb5aee77d099d7263f1783d678692df88b263
filippottaviani <filippo.ottaviani.tr@gmail.com>
2025-01-23 23:46:40 +0100

    Aggiunta del monitoraggio della loss e di ReduceLROnPlateau

--- solver.py
+++ solver.py
@@ -52,12 +52,13 @@ class Solver:
             self.criterion_rec1 = torch.nn.MSELoss() # prova
 
             # ottimizzatori
-            self.optimizer_G = torch.optim.Adam(self.globalnet.parameters(), lr=args.lr, betas=(0.9,0.999))
+            self.optimizer_G = torch.optim.Adam(self.globalnet.parameters(), lr=args.lr, betas=(0.9, 0.999))
             self.optimizer_R = torch.optim.SGD(self.refnet.parameters(), lr=args.lr, momentum=0.9, weight_decay=0.0005)
             self.optimizer_D = torch.optim.SGD(self.discriminator.parameters(), lr=args.lr, momentum=0.9, weight_decay=0.0005)
 
             # scheduling di riduzione della LR
             self.scheduler_R = ReduceLROnPlateau(self.optimizer_R, mode='min', factor=0.5, patience=5)
+            self.scheduler_G = ReduceLROnPlateau(self.optimizer_G, mode='min', factor=0.5, patience=5)
 
             # pesi delle loss
             self.lambda_start = 5
@@ -69,7 +70,13 @@ class Solver:
             self.set = DepthDataset.TRAIN
             #self.set = DepthDataset.VAL
 
-            # salvataggio delle metriche  addestramento adversarial
+            # salvataggio delle loss durante gli addestramenti
+            self.glb_train_loss = []
+            self.pre_train_loss = []
+            self.adv_r_train_loss = []
+            self.adv_d_train_loss = []
+
+            # salvataggio delle metriche addestramento adversarial
             self.adv_train_ssim_epochs = []
             self.adv_train_rmse_epochs = []
 
@@ -91,6 +98,7 @@ class Solver:
         print("GLOBAL FITTING")
         for epoch in range(self.args.max_epochs):
             self.globalnet.train()
+            total_g_loss = 0
 
             for images, depth in self.train_loader:
                 images, depth = images.to(self.device), depth.to(self.device)
@@ -99,8 +107,13 @@ class Solver:
                 g_loss = self.criterion_rec(fake_depth, depth)
 
                 self.optimizer_G.zero_grad()
+                total_g_loss += g_loss.item()
                 g_loss.backward()
                 self.optimizer_G.step()
+
+            # aggiornamento della loss
+            avg_g_loss = total_g_loss / len(self.train_loader)
+            self.scheduler_G.step(avg_g_loss)
 
             # monitoraggio addestramento
             time = datetime.datetime.now()
@@ -109,18 +122,21 @@ class Solver:
                                                                          curr_clock))
             epoch += 1
 
+            # per valutazione andamento della loss
+            self.glb_train_loss.append(g_loss)
+
             if epoch % 10 == 0:
                 self.global_evaluate()
 
             # salvataggio del checkpoint
             if epoch == self.args.max_epochs:
                 self.save_global(self.args.gl_ckpt_dir, self.args.gl_ckpt_name, epoch)
-                print("GLOBAL FITTING TERMINATO")
 
     def refnet_pretrain(self):
         print("\n\nREFINEMENT NET PRE-TRAINING")
         gl_ckpt_file = os.path.join("checkpoint", "global", self.args.gl_ckpt_file)
-        self.globalnet.load_state_dict(torch.load(gl_ckpt_file, weights_only=True)) # per evitare di riaddestrare ogmi volta la global
+        self.globalnet.load_state_dict(torch.load(gl_ckpt_file, weights_only=True))  # per evitare di riaddestrare ogmi volta la global
+        total_r_loss = 0
 
         for epoch in range(10):
             self.refnet.train()
@@ -134,7 +150,12 @@ class Solver:
                 # addestramento refinement
                 self.optimizer_R.zero_grad()
                 ref_loss.backward()
+                total_r_loss += ref_loss.item()
                 self.optimizer_R.step()
+
+            # aggiornamento della loss
+            avg_r_loss = total_r_loss / len(self.train_loader)
+            self.scheduler_R.step(avg_r_loss)
 
             # monitoraggio addestramento
             time = datetime.datetime.now()
@@ -142,9 +163,12 @@ class Solver:
             print("Epoch [{}/10] Loss R: {:.3f} Time:{}".format(epoch + 1, ref_loss, curr_clock))
             epoch += 1
 
+            # per valutazione andamento della loss
+            self.pre_train_loss.append(ref_loss)
+
             # salvataggio del checkpoint
             if epoch == 10:
-                self.adv_evaluate() # metriche prima dell'addestramento avversario
+                self.adv_evaluate()  # metriche prima dell'addestramento avversario
                 self.save_refinenment_final(self.args.ref_ckpt_dir, self.args.ref_ckpt_name, epoch)
 
     def adversarial_fit(self):
@@ -155,7 +179,7 @@ class Solver:
         for epoch in range(self.args.max_epochs):
             self.refnet.train()
             self.discriminator.train()
-            total_loss = 0
+            total_r_loss = 0
             lambda_adv = self.lambda_start  # valore iniziale
             alpha = 0.05  # velocità di adattamento
 
@@ -195,19 +219,19 @@ class Solver:
                 ref_loss2 = ssim(fake_depth, cr_depths)
 
                 # aggiornamento lambda_rec
-                ratio = rec_loss.item() / (adv_loss.item()+ 1e-8)
-                lambda_adv *= ratio ** alpha
-                lambda_adv = max(0.01, min(lambda_adv, 5.0))
+                #ratio = rec_loss.item() / (adv_loss.item()+ 1e-8)
+                #lambda_adv *= ratio ** alpha
+                #lambda_adv = max(0.01, min(lambda_adv, 5.0))
 
                 # calcolo della loss
                 r_loss = rec_loss + lambda_adv * adv_loss + ref_loss1 + 1/ref_loss2
-                total_loss += r_loss.item()
+                total_r_loss += r_loss.item()
                 r_loss.backward()
                 self.optimizer_R.step()
 
             # aggiornamento della loss
-            avg_loss = total_loss / len(self.train_loader)
-            self.scheduler_R.step(avg_loss)
+            avg_r_loss = total_r_loss / len(self.train_loader)
+            self.scheduler_R.step(avg_r_loss)
 
             # monitoraggio addestramento
             time = datetime.datetime.now()
@@ -219,12 +243,15 @@ class Solver:
                                                                                                 curr_clock))
             epoch += 1
 
-            #if epoch % 10 == 0:
-                #self.adv_evaluate()
+            # per valutazione andamento della loss
+            self.adv_r_train_loss.append(r_loss)
+            self.adv_d_train_loss.append(d_loss)
+
+            if epoch % 10 == 0:
+                self.adv_evaluate()
 
             if epoch == self.args.max_epochs:
                 self.save_refinenment_final(self.args.ref_ckpt_dir, self.args.ref_ckpt_name, epoch)
-                self.adv_evaluate()
                 plot_metrics(self)
 
     def global_evaluate(self):
